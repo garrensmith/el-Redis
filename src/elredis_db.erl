d0a0be66c1bfe10cb4dc812cb42f3eb1b5789e24
Garren Smith <garren.smith@gmail.com>
2012-03-07 14:18:16 +0200

    can do basic increment on string

--- src/elredis_db.erl
+++ src/elredis_db.erl
@@ -1,18 +1,21 @@
 -module(elredis_db).
 -behaviour(gen_server).
 
--export([start_link/0, stop/0, add/2, lookup/1]).
+-export([start_link/0, stop/0, set/2, lookup/1, incrby/2]).
 
 %% gen_server callbacks
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 
 -record(state, {}).
 
-add(Key,Value) ->
-  gen_server:call(?MODULE, {add,{Key,Value}}).
+set(Key,Value) ->
+  gen_server:call(?MODULE, {set,{Key,Value}}).
 
-lookup(_Key) ->
-  ok.
+lookup(Key) ->
+  gen_server:call(?MODULE, {lookup,Key}).
+
+incrby(Key,Amount) ->
+  gen_server:call(?MODULE, {incrby, Key, Amount}).
 
 stop() ->
   gen_server:call(?MODULE, stop).
@@ -24,9 +27,21 @@ init([]) ->
   ets:new(elredis_set, [set, named_table, protected]),
   {ok, #state{}}.
 
-handle_call({add, {Key, Value}}, _From, State) ->
-    ets:insert(elredis_set,{Key,Value}),
-    {reply, ok, State};
+handle_call({set, {Key, Value}}, _From, State) ->
+  ets:insert(elredis_set,{Key,Value}),
+  {reply, ok, State};
+
+handle_call({lookup, Key}, _From, State) ->
+  Result = ets:lookup(elredis_set,Key),
+  {reply, Result, State};
+
+handle_call({incrby, Key, Amount}, _From, State)  ->
+  Value = ets:lookup_element(elredis_set,Key,2),
+  IncrementedValue = list_to_integer(Value) + Amount,
+  ets:delete(elredis_set, Key),
+  ets:insert(elredis_set,{Key, IncrementedValue}),
+  {reply, IncrementedValue, State};
+
 handle_call(stop, _From, Tab) ->
   {stop, normal, stopped, Tab};
 handle_call(_Request, _From, State) ->
--- src/elredis_db_tests.erl
+++ src/elredis_db_tests.erl
@@ -1,21 +1,46 @@
 -module(elredis_db_tests).
 -include_lib("eunit/include/eunit.hrl").
 
-store_data_test_() ->
-  {setup,
+store_retrieve_set_data_test_() ->
+  {foreach,
     fun setup/0,
     fun teardown/1,
-    fun should_add_item/1}.
+    [fun should_add_item/1,
+     fun should_retrieve_item_with_key/1 ]}.
+
+increment_value_test_() ->
+  {foreach,
+    fun setup/0,
+    fun teardown/1,
+    [fun should_increment_by_2/1,
+     fun should_check_if_key_exists/1]}.
 
 setup() ->
   elredis_db:start_link().
-
+  
 teardown(_Info) ->
   elredis_db:stop().
 
 should_add_item(_Info) ->
-  Response = elredis_db:add(hello,world),
-  [?_assertEqual(Response, ok)].
+  Response = elredis_db:set(hello,world),
+  [?_assertEqual(ok, Response)].
+
+should_retrieve_item_with_key(_Info) ->
+  elredis_db:set(coffee,yum),
+  LookUpValue = elredis_db:lookup(coffee), 
+  [?_assertEqual([{coffee,yum}], LookUpValue)].
+
+should_increment_by_2(_Info) ->
+  elredis_db:set(counter, "100"),
+  IncreasedValue = elredis_db:incrby(counter, 2),
+  [{counter, CounterValue}|_] = elredis_db:lookup(counter),
+  [?_assertEqual(102, IncreasedValue),
+   ?_assertEqual(102, CounterValue)].
+
+should_check_if_key_exists(_Info) ->
+  IncreasedValue = elredis_db:incrby(counter, 2),
+  [?_assertEqual(102, IncreasedValue)].
+
 
 
 
