fd128d96229236ea25c9e160e303a8c0e75f4ae5
Garren Smith <garren.smith@gmail.com>
2012-03-23 19:04:20 +0200

    partial implementation of get

--- src/elredis_message_handler.erl
+++ src/elredis_message_handler.erl
@@ -10,12 +10,10 @@ process_message(Message) ->
   case binary:split(Message,<<"\r\n">>) of
     [<<"*",LengthBin/binary>>, Rest] -> 
       Length = list_to_integer(binary_to_list(LengthBin)), 
-      io:format("L ~p ~n",[Length]),
       Commands = split_commands(Rest),
-      io:format("Rest ~p ~n",[Rest]),
       process_bulk_message(Length, Commands);
     _ -> process_bulk_message(1,error)
-   end.
+  end.
 
 split_commands(Buffer) ->
   split_commands(Buffer,[]).
@@ -33,11 +31,19 @@ process_bulk_message(1, ["PING"]) ->
   <<"+PONG\r\n">>;
 process_bulk_message(1,["INFO"]) ->
   <<"+elredis:0.0.1\r\n\r\n">>;
+process_bulk_message(3,["SET",Key, Value]) ->
+  case elredis_db:set(Key, Value) of
+    ok -> <<"+OK\r\n">>;
+    _ -> <<"-error occured">>
+  end;
+process_bulk_message(2,["GET",Key]) ->
+  %[{Key, Value}] = elredis_db:lookup(Key),
+  <<"$5\r\nhello\r\n">>;%Value;
 process_bulk_message(_, _) ->
   <<"-ERR Unknown or disabled command\r\n">>.
 
 %%
 %%%X = "*3\r\n$3\r\nSET\r\n$5\r\nmykey\r\n$7\r\nmyvalue\r\n",
 %%  X="+PONG\r\n",
-  %X = <<"*1\r\n$19\r\nredis_version:2.2.2\r\n">>,
+%X = <<"*1\r\n$19\r\nredis_version:2.2.2\r\n">>,
 
--- src/elredis_message_handler_tests.erl
+++ src/elredis_message_handler_tests.erl
@@ -2,23 +2,34 @@
 -include_lib("eunit/include/eunit.hrl").
 
 setup() ->
-  ok.
-
-teardown(_info) ->
-  ok.
+  elredis_db:start_link().
+  
+teardown(_Info) ->
+  elredis_db:stop().
 
 store_retrieve_set_data_test_() ->
   {foreach,
     fun setup/0,
     fun teardown/1,
    [fun should_reply_to_ping_message/1,
-    fun should_reply_to_lower_case_ping_message/1]}.
+    fun should_reply_to_lower_case_ping_message/1,
+    fun should_set_key_and_respond_with_ok/1,
+    fun should_get_set_key_value/1]}.
 
-should_reply_to_ping_message(_info) ->
+should_reply_to_ping_message(_Info) ->
   Response = elredis_message_handler:process_message(<<"*1\r\n$4\r\nPING\r\n">>),
   [?_assertEqual(<<"+PONG\r\n">>, Response)].
 
-should_reply_to_lower_case_ping_message(_info) ->
+should_reply_to_lower_case_ping_message(_Info) ->
   Response = elredis_message_handler:process_message(<<"*1\r\n$4\r\nping\r\n">>),
   [?_assertEqual(<<"+PONG\r\n">>, Response)].
+
+should_set_key_and_respond_with_ok(_Info) ->
+  Response = elredis_message_handler:process_message(<<"*3\r\n$3\r\nset\r\n$5\r\nmykey\r\n$5\r\nhello\r\n">>),
+  [?_assertEqual(<<"+OK\r\n">>, Response)].
+
+should_get_set_key_value(_Info) -> 
+  elredis_message_handler:process_message(<<"*3\r\n$3\r\nset\r\n$5\r\nmykey\r\n$5\r\nhello\r\n">>),
+  Response = elredis_message_handler:process_message(<<"*2\r\n$3\r\nget\r\n$5\r\nmykey\r\n">>),
+  [?_assertEqual(<<"$5\r\nhello\r\n">>, Response)].
 
